721e68f43b6a75c78b7f55bf66cf5c2f6f42c211
David Christino <david.silva@supermercadosbh.com.br>
2025-09-16 16:07:03 -0300

    Testing controller with FromQuery

--- OrderService/Controllers/Customer/HomeController.cs
+++ OrderService/Controllers/Customer/HomeController.cs
@@ -21,10 +21,22 @@ public class HomeController(ICustomersRepository repository, AppDBContext appCon
     [HttpGet, Route("id")]
     [ProducesResponseType(200, Type = typeof(IEnumerable<Models.Customer>))]
     [ProducesResponseType(400, Type = typeof(IEnumerable<Models.Customer>))]
-    public async Task<ActionResult> GetCustomerIdAsync(long id, CancellationToken cancellationToken)
+    public async Task<ActionResult> GetCustomerIdAsync([FromQuery] long id, CancellationToken cancellationToken)
     {
         var result = await repository.GetCustomerByIdAsync(id, cancellationToken);
 
-        return Ok(result);
+        return result == null ? NotFound("Notting was found please check if the user exists") : Ok(result);
+    }
+
+    [HttpGet, Route("name")]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<Models.Customer>))]
+    [ProducesResponseType(400, Type = typeof(IEnumerable<Models.Customer>))]
+    public async Task<ActionResult> GetNameAsync([FromQuery] List<string> name, CancellationToken cancellationToken)
+    {
+        var result = await repository.GetCustomerByNameAsync(name, cancellationToken);
+
+        return result.Count == 0
+            ? NotFound("Notting was found please check if the user exists")
+            : Ok(result);
     }
 }
